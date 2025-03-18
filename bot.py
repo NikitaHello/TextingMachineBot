d7c5d81e6ffab274277d9643a1a676865293b49e
troubledjoe <pchelkin@me.com>
2025-03-18 21:46:04 +0300

    message sending functionality

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ API_KEY = os.getenv("API_KEY")
 
 messages = []
 
-async def trackchat(update: Update , context: ContextTypes.DEFAULT_TYPE) -> None:
+async def trackchat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     global messages
     sender = update.effective_message.from_user.first_name
     text = update.effective_message.text
@@ -20,15 +20,16 @@ async def trackchat(update: Update , context: ContextTypes.DEFAULT_TYPE) -> None
     if len(messages) >= 100:
         messages_json = json.dumps(messages, ensure_ascii=False, indent=2)
 
-    await process_messages(messages)
+    await process_messages(messages, update.effective_message.chat_id)
 
     messages.clear()
 
-async def process_messages(messages_json: str) -> None:
+async def process_messages(messages_json: str, chat_id: int) -> None:
     client = genai.Client(api_key=API_KEY)
     response = client.models.generate_content(
-        model="gemini-2.0-flash", contents="Your response have to be less than 100 symbols. Behave if you were a human Imperor from Warhammer 40000 and taking part in coference of Primarchs. Here's what they say:\n"+messages_json.text
+        model="gemini-2.0-flash", contents="Your response have to be less than 100 symbols. Behave if you were a human Imperor from Warhammer 40000 and taking part in conference of Primarchs. Here's what they say:\n"+messages_json.text
     )
+    await context.bot.send_message(chat_id, text = response.text) #вроде должно работать...
     print(response.text)
 
 def main() -> None:
