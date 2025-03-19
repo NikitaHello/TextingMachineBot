9ba1462ab325cbd2df9f1d8fb894f81f2b68d432
Nikita <klassgordon@gmail.com>
2025-03-19 20:54:07 +0300

    deleted change

--- bot.py
+++ bot.py
@@ -19,15 +19,14 @@ async def trackchat(update: Update , context: ContextTypes.DEFAULT_TYPE) -> None
     messages.append({"sender": sender, "text": text})
     if len(messages) >= 100:
         messages_json = json.dumps(messages, ensure_ascii=False, indent=2)
-
-    await process_messages(messages)
+        await process_messages(messages_json)
 
     messages.clear()
 
 async def process_messages(messages_json: str) -> None:
     client = genai.Client(api_key=API_KEY)
     response = client.models.generate_content(
-        model="gemini-2.0-flash", contents="Your response have to be less than 100 symbols. Behave if you were a human Imperor from Warhammer 40000 and taking part in coference of Primarchs. Here's what they say:\n"+messages_json.text
+        model="gemini-2.0-flash", contents="Your response have to be less than 100 symbols. Behave if you were a human Imperor from Warhammer 40000 and taking part in coference of Primarchs. Here's what they say:\n"+messages_json
     )
     print(response.text)
 
