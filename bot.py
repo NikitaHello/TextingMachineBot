1265c5dfc71a66a326fea47555f785ceed6ae06a
troubledjoe <pchelkin@me.com>
2025-03-23 18:29:33 +0300

    further fixes

--- bot.py
+++ bot.py
@@ -89,12 +89,8 @@ async def get_random_prompt():
     prompts = await prompts_db.all()
     if not prompts:
         return "Placeholder prompt. Behave as you wish."
-<<<<<<< HEAD
-    return random.choice(prompts).template
-=======
     templates = [t["template"] for t in prompts]
     return random.choice(templates)
->>>>>>> 33509cf (further fixes)
 
 def clamp(val, min_val, max_val):
     return max(min_val, min(val, max_val))
